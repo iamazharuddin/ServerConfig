218684287f9f42511db03fcaa72aa4a3347f40b6
iamazharuddin <iamazharuddin10@gmail.com>
2023-06-25 14:55:28 +0530

    cerate different flavor

--- ServerConfig/ServerConfig.swift
+++ ServerConfig/ServerConfig.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+import Foundation
+enum BaseUrl : String{
+     case qaDebug = "https:local.com"
+     case prodDebug = "https:prod.com"
+     case debug = "https:debug.com"
+}
+
+
+class ServerConfig{
+     static let shared = ServerConfig()
+     var baseUrl : String?
+     func setupConfig(){
+         #if qa
+           self.baseUrl = BaseUrl.qaDebug.rawValue
+         #elseif prod
+           self.baseUrl = BaseUrl.prodDebug.rawValue
+         #elseif DEBUG
+          self.baseUrl = BaseUrl.debug.rawValue
+         #endif
+         print(self.baseUrl ?? "not found")
+     }
+}
